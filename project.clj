a670b460b46f078c61259f7fdbcf0e5c51a29e63
Sean Lee <freshdried@gmail.com>
2016-02-03 14:25:02 -0500

    added markdown editor

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject hello-reagent "0.1.0-SNAPSHOT"
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.228"]
-                 [reagent "0.6.0-alpha"]]
+                 [reagent "0.6.0-alpha"]
+                 [markdown-clj "0.9.85"]]
+
   :plugins [[lein-figwheel "0.5.0-1"]]
   :clean-targets [:target-path "out" "resources/public/cljs"]
   :cljsbuild {
--- src/hello/core.cljs
+++ src/hello/core.cljs
@@ -1,11 +1,43 @@
 (ns hello.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [markdown.core :refer [md->html]]))
 
-(defonce counter (r/atom 0))
+(defonce counter-state (r/atom 0))
+(defonce text-state (r/atom "#yo!\nWrite some **markdown** in me!"))
 
-(defn app[]
-  [:div {:on-click #(swap! counter inc)}
-   "I have been clicked " @counter " times."])
+(.log js/console
+      (md->html "###This is a heading\n with a p!"))
+
+(defn counter []
+  [:div {:on-click #(swap! counter-state inc)}
+   "I have been clicked " @counter-state " times."])
+
+(defn dangerous [text]
+  [:div {:dangerouslySetInnerHTML {:__html (md->html text)}}])
+
+(def text "
+  # Yo!
+  Welcome to my hello worldliness.
+")
+
+(defn editor []
+  [:div
+   [:textarea {:rows "10"
+               :cols "50"
+               :placeholder "Yo write some markdown in me!"
+               :value @text-state
+               :on-change #(reset! text-state (-> % .-target .-value))}]
+   [:div [dangerous @text-state]]])
+
+
+(defn app []
+  [:div
+   [:div [dangerous text]]
+   [:h3 "counter"]
+   [counter]
+   [:br] [:br] [:br] [:br]
+   [:h3 "markdown editor"]
+   [editor]])
 
 
 (defn start! []
